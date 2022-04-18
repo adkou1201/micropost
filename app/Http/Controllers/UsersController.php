664e66c9568ce67bf69942f0f00fc7c93007e8b7
adkou1201 <kou.1201.d@gmail.com>
2022-04-18 14:02:32 +0000

    post

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\User; // 追加
+use App\User;
 
 class UsersController extends Controller
 {
@@ -24,10 +24,17 @@ class UsersController extends Controller
     {
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
+        
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+        
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        // ユーザ詳細ビューでそれを表示
+        // ユーザ詳細ビューでそれらを表示
         return view('users.show', [
             'user' => $user,
+            'microposts' => $microposts,
         ]);
     }
     
--- app/User.php
+++ app/User.php
@@ -36,4 +36,17 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+    
+    
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }
+   
 }
